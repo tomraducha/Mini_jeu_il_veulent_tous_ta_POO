e31de47febc4ae3212e3a2d9f303614c82b7b534
Thomas Raducha <thomasraducha@gmail.com>
2022-10-18 20:12:27 +0200

    save 3

--- lib/player.rb
+++ lib/player.rb
@@ -1,30 +1,34 @@
+# On créer notre classe Player
 class Player
   attr_accessor :name, :life_points
 
+  # Initialise le constructor
   def initialize(name, life_points)
     @name = name
     @life_points = life_points
   end
 
+  # affiche l'état d'un joueur
   def show_state
     puts("Le joueur #{@name} a #{@life_points} points de vie")
   end
 
+  # Applique le domage au joueur
   def get_damage(damage)
     @life_points -= damage
-    if @life_points <= 0
-      @life_points = 0
-      puts('Le joueur a été tué!!!!')
-    end
+    puts('Le joueur a été tué!!!!') if @life_points <= 0
   end
 
+  # Lance une attaque
   def attacks(player)
     player.get_damage(compute_damage)
+    dmg = compute_damage
     puts("Le joueur #{name} attaque le joueur #{player.name}")
-    puts("il lui inflige #{compute_damage} points de dommages")
+    puts("il lui inflige #{dmg} points de dommages")
   end
 
+  # Dommages aléatoires
   def compute_damage
-    return rand(1..6)
+    return rand(0..6)
   end
 end
